0f358cdfa78ae49d9903b24479a20795772293fb
mpandolfelli <martinpandolfelli@gmail.com>
2016-01-20 21:08:02 -0300

    empiezo maqueta buscador

--- application/views/layout_web.php
+++ application/views/layout_web.php
@@ -12,8 +12,7 @@
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	<link href='https://fonts.googleapis.com/css?family=Raleway:400,500,700' rel='stylesheet' type='text/css'>
 	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -124,12 +123,12 @@
 				  	</div>
 				</nav>
 		
-		<div class="row">
+		
+    </div>
+    <div class="container-fluid no-padding">
 		<!-- Content -->
 		<?php echo $content_for_layout?>
-
-		<!-- / Content -->
-		</div>
+		<!-- / Content -->		
     </div>
  	<!-- / Main container -->
 
