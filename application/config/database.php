dfe5e144ec73b71864eabd59fda144ad24a75c78
Hugo <hugomarcelo@gmail.com>
2016-03-03 11:40:31 -0300

    cambio titulo de contactos

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $query_builder = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = '1q2w3e';
 $db['default']['database'] = 'w6000506_nuevo';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
