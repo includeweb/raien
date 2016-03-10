64e8c73f63ab13e3bf66ca4848945d7acafab641
martinpandolfelli@gmail.com <martinpandolfelli@gmail.com>
2016-03-10 16:34:41 -0300

    revierto cambios

--- application/views/site/product_detail.php
+++ application/views/site/product_detail.php
@@ -173,7 +173,7 @@
 			var json = $.parseJSON(data);
 
 			productosRelacionados = json['productos'];
-			var brandImg = '<img src="<?=base_url();?>images/productos/logos/brands/'+json.productos[0].imagen+'.jpg" />';
+			var brandImg = '<img src="<?=base_url();?>images/productos/logos/'+json.productos[0].imagen+'.png" />';
 			var brandText = '<div>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>';
 			
 			
